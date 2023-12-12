feca9ef49250c46a06602cd59cf78bbc34da7fac
Denis Rodriguez <denis_design@live.cl>
2023-12-12 10:10:28 -0400

    Home Screen implementado

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,9 +7,55 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(        
+      appBar: AppBar(
         title: const Text("Home Screen"),
       ),
+      body: const _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      // Utilizaremos el <appMenuItems> definido anteriormente
+      itemCount: appMenuItems.length,
+      itemBuilder: (context, index) {
+        final item = appMenuItems[index];
+        return _CustomListTile(item: item);
+      },
+    );
+  }
+}
+
+class _CustomListTile extends StatelessWidget {
+  const _CustomListTile({
+    required this.item,
+  });
+
+  final MenuItem item;
+
+  @override
+  Widget build(BuildContext context) {
+    final color = Theme.of(context).colorScheme;
+
+    return ListTile(
+      leading: Icon(
+        item.icon,
+        color: color.primary,
+      ),
+      trailing: Icon(
+        Icons.arrow_forward_ios_sharp,
+        color: color.primary,
+      ),
+      title: Text(item.title),
+      subtitle: Text(item.subTitle),
+      onTap: () {
+        // TODO navegar a otra pantalla
+      },
     );
   }
 }
