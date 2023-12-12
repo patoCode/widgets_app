9c3d47d37336678baf366760e44899bc026dfe3b
Denis Rodriguez <denis_design@live.cl>
2023-12-12 09:54:58 -0400

    Modifique los estilos del appbar, especificamente se centraron los titles

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -1,5 +1,35 @@
 import 'package:flutter/material.dart';
 
+const colorList = <Color>[
+  Colors.blue,
+  Colors.teal,
+  Colors.green,
+  Colors.red,
+  Colors.purple,
+  Colors.pink,
+  Colors.deepPurple,
+  Colors.orange,
+  Colors.indigo,
+];
+
 class AppTheme {
-  ThemeData getTheme() => ThemeData();
+  final int selectedColor;
+
+  AppTheme({
+    this.selectedColor = 0,
+  })  : assert(
+          selectedColor >= 0,
+          "Selected color must be greater then 0",
+        ),
+        assert(
+          selectedColor < colorList.length,
+          "Selected color must be less or equal then ${colorList.length - 1}",
+        );
+  ThemeData getTheme() => ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: colorList[selectedColor],
+        appBarTheme: const AppBarTheme(
+          centerTitle: true,
+        ),
+      );
 }
