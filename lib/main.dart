ad99e7f7fc51abf664b6152899467b26459f13aa
Denis Rodriguez <denis_design@live.cl>
2023-12-13 11:33:36 -0400

    Implementacion de RiverPOd en provider terminado.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:widgets_app/config/router/router_app.dart';
 import 'package:widgets_app/config/theme/app_theme.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+    // ** <ProviderScope> Con este widget RIVERPOD ya sabe donde buscar los providers
+    // ** Simpre debe estar incluido en esta seccion, despues de instalar <flutter_riverpod>
+    const ProviderScope(
+      child: MainApp(),
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget {
--- lib/presentation/screens/counter/counter_screen.dart
+++ lib/presentation/screens/counter/counter_screen.dart
@@ -1,28 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:widgets_app/presentation/providers/counter_provider.dart';
+import 'package:widgets_app/presentation/providers/theme_provider.dart';
 
-class CounterScreen extends StatelessWidget {
+// ** <ConsumerWidget> Permite a Riverpod hacer seguimiento a un StatelessWidget. Al final se podria decir que son lo mismo, solo que Riverpod arega funcionalidad para incluir la funcionalidad de provider a este widget (ver doc. Riverpod)
+class CounterScreen extends ConsumerWidget {
   static const String name = 'counter_screen';
 
   const CounterScreen({super.key});
 
+  // ** <WidgetRef ref>  Este parametro en el build, debe ser incluido cuando se convierte en un ConsumerWidget, es obligatorio!!!
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final textStyle = Theme.of(context).textTheme.titleLarge;
+
+    // ** registramos el provider que vamos a monitorizar en este caso [counterProvider]
+    // !! watch no se debe usar dentro de metodos
+    final int clickValue = ref.watch(counterProvider);
+    // ** gestor del darkmode action
+    final bool isDarkMode = ref.watch(themeProvider);
 
     return Scaffold(
       appBar: AppBar(
         title: const Text(
           "Counter screen",
         ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 20),
+            child: IconButton(
+              onPressed: () {
+                // ** OPTION 1
+                // ref.read(themeProvider.notifier).state = !ref.read(themeProvider.notifier).state;
+                // ** OPTION 2 Para este caso queda mas entendible esto
+                ref.read(themeProvider.notifier).update(
+                      (isDarkMode) => !isDarkMode,
+                    );
+              },
+              icon: isDarkMode
+                  ? const Icon(Icons.dark_mode_outlined)
+                  : const Icon(Icons.light_mode_outlined),
+            ),
+          ),
+        ],
       ),
       body: Center(
         child: Text(
-          "Contador valor: 10",
+          "Contador valor: $clickValue",
           style: textStyle,
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          // ** De esta forma con Riverpord accedemos al estado del proviuder y modificamos el mismo, notifier es obligatorio
+          ref.read(counterProvider.notifier).state++;
+          // ** ALTERNATIVA a la actualizacion de estado, es indistinto cual se use
+          // ref.read(counterProvider.notifier).update((state) => state++);
+        },
         child: const Icon(Icons.plus_one_outlined),
       ),
     );
